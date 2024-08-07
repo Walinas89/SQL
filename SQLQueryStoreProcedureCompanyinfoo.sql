36aa4a03352a458b1fb0ef1d0ce11df6ce56a965
Walinas89 <nasiruwaliat9@gmail.com>
2024-08-06 17:17:16 -0700

    Add files via upload

--- SQLQueryStoreProcedureCompanyinfoo.sql
+++ SQLQueryStoreProcedureCompanyinfoo.sql
@@ -7,3 +7,16 @@ SELECT [FirstName]
   FROM [CompanyInfoo].[dbo].[CompanyInfoo]
 
   Exec SpPullCompanyInfooData
+
+  ---Creating  a Backup Table
+  Select * Into CompanyInfooBackup
+  From CompanyInfoo
+
+---- Fetching all Records
+Select *
+From CompanyInfooBackup
+SELECT [FirstName]
+      ,[LastName]
+      ,[Country]
+  FROM [CompanyInfoo].[dbo].[CompanyInfoo]
+
